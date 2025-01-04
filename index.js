0c6c22fe004bcf0367aa394c764b174dea07bd9a
fanbyprinciple <fanbyprinciple@gmail.com>
2025-01-04 07:16:20 +0530

    adding ujwal

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const product = document.querySelectorAll(".product");
-const circle = document.querySelector(".scroll-progress");
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-window.onscroll = function () {
-  const element = document.querySelector(".header");
-
-  let progress =
-    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-  circle.innerText = Math.round(progress) + "%";
-
-  if (window.scrollY > 10) {
-    element.classList.add("posColor");
-  } else {
-    element.classList.remove("posColor");
-  }
-};
-
-window.addEventListener("scroll", scroller);
-
-function scroller() {
-  const size = (window.innerHeight / 5) * 4;
-
-  product.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
-
-    if (boxTop < size) {
-      box.classList.add("scroll");
-    } else {
-      box.classList.remove("scroll");
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
     }
-  });
 }
+
+showMenu('nav-toggle','nav-menu');
+
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
+
+gsap.from(".home-img", {opacity: 0, duration: 2, delay: 2, x: 60});
+
+gsap.from(".home-information", {opacity: 0, duration: 3, delay: 2.3, y: 25});
+gsap.from(".anime-text", {opacity: 0, duration: 3, delay: 2.3, y: 25, ease: "expo.out", stagger: .3});
+
+gsap.from(".nav-logo", {opacity: 0, duration: 3, delay: 3.2, y: 25, ease: "expo.out"});
+gsap.from(".nav-item", {opacity: 0, duration: 3, delay: 3.2, y: 25, ease: "expo.out", stagger: .2});
+
+gsap.from(".home-social", {opacity: 0, duration: 3, delay: 4, y: 25, ease: "expo.out", stagger: .2});
+
